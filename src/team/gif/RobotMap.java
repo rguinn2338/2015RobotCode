8daa2f1d31652ef998fc4338f5703c54649400e3
goldenspeedster <Patrick@Patricks-MacBook-Pro.local>
2015-01-31 11:35:34 -0600

    RobotMap Update

--- src/team/gif/RobotMap.java
+++ src/team/gif/RobotMap.java
@@ -7,33 +7,29 @@ package team.gif;
 public class RobotMap {
 	
 	// CAN addresses
-    public static final int frontLeft = 0;
-    public static final int frontRight = 1;
-    public static final int rearLeft = 2;
-    public static final int rearRight = 3;
-    public static final int collectorLeft = 4;
-    public static final int collectorRight = 5;
-    public static final int elevator = 6;
-    public static final int totePusher = 7;
-    public static final int toteGrabber = 8;
+    public static final int frontLeft = 1;
+    public static final int frontRight = 2;
+    public static final int rearLeft = 3;
+    public static final int rearRight = 4;
+    public static final int collectorLeft = 5;
+    public static final int collectorRight = 6;
+    public static final int elevator = 7;
+    public static final int totePusher = 8;
     
     // DIO ports
     public static final int elevatorA = 4;
     public static final int elevatorB = 5;
     
     // Relay ports (solenoids)
-    // Based off the list found here (1/21/15): http://oswegofirst.org/forum/viewtopic.php?f=12&t=2968
     public static final int collectorA = 0;
     public static final int collectorB = 1;
-    public static final int leftRCArmA = 2;
-    public static final int leftRCArmB = 3;
-    public static final int rightRCArmA = 4;
-    public static final int rightRCArmB = 5;
+    public static final int leftRCarmA = 2;
+    public static final int leftRCarmB = 3;
+    public static final int rightRCarmA = 4;
+    public static final int rightRCarmB = 5;
     public static final int hooksA = 6;
     public static final int hooksB = 7;
-    public static final int chopSticks = 8;
-    
-    // Analog ports
-    public static final int gyroRate = 0; // Gyro MUST be on either 0 or 1
+    public static final int chopSticksA = 8;
+    public static final int chopSticksB = 9;
     
 }
